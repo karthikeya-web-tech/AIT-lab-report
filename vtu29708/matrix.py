b2d48b5996ee50f2b4d57ec0dff7ed7c401ffc0c
karthikeya-web-tech <vtu29708@veltech.edu.in>
2025-09-23 11:10:41 +0530

    Add files via upload

---
+++ vtu29708/matrix.py
@@ -0,0 +1,12 @@
+matrix=[
+        [0,0,0],
+        [0,1,0],
+        [0,0,0]
+]
+
+for row in matrix:
+    for element in row:
+        print(element, end=' ')
+    print()                               
+
+
